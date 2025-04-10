a4cf51dda9e27d1379845aa1111a5cd0bc69608d
nanwix <renancaueassis2805@gmail.com>
2025-04-10 15:05:01 -0300

    Add files via upload

---
+++ exerc3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	char letra, nome[10];
+	printf("digite uma letra qualquer \n");
+	scanf("%c",&letra);
+	printf("digite um nome \n");
+	scanf("%s",&nome);
+	printf("\n Letra informada = %c",letra);
+	printf("\n Nome informada = %s", nome);
+	
+}
