68df71c814a57f1064c6c9e65ae7570436235ae1
nanwix <renancaueassis2805@gmail.com>
2025-04-10 14:34:17 -0300

    Add files via upload

---
+++ exerc2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	float n1, n2, n3, media;
+	printf("Informe valor 1 \n");
+	scanf("%f", &n1);
+	printf("Informe valor 2 \n");
+	scanf("%f", &n2);
+	printf("Informe valor 3 \n");
+	scanf("%f", &n3);
+	media=n1+n2+n3/2;
+	printf("Media é: %f", media);	
+}
