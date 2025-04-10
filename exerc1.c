1344a8c4f8829b43b6ebdbab65866b4bb12a2912
nanwix <renancaueassis2805@gmail.com>
2025-04-10 14:24:21 -0300

    Add files via upload

---
+++ exerc1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+   float n1, n2, soma;
+   printf("Informe valor 1 \n");
+   scanf( "%f", &n1);
+   printf("Informe valor 2\n");
+   scanf( "%f", &n2);
+   soma=n1+n2;
+   printf(" %f Soma é:",  soma);
+}
